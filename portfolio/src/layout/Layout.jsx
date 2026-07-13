f6fecad4568676740089ce6b8e31b17ee112700a
George Kihoro <georgekihoro6@gmail.com>
2026-07-13 23:27:28 +0300

    Refactor Layout component structure for improved readability

--- portfolio/src/layout/Layout.jsx
+++ portfolio/src/layout/Layout.jsx
@@ -1,3 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+
+export default function Layout(){
+    return (
+        <div className="site">
+            <Navbar/>
+            <main className="site-main">
+                <Outlet/>
+            </main>
+            <Footer/>
+        </div>
+    );
+        
+}
