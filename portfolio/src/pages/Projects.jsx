728cbfb1a3178a80cd0ca2fc94b8b136f07b18cd
George Kihoro <georgekihoro6@gmail.com>
2026-07-14 12:03:34 +0300

    Add PI_STUDIES constant with details of research projects on women's health

---
+++ portfolio/src/pages/Projects.jsx
@@ -0,0 +1,38 @@
+const PI_STUDIES = [
+  {
+    tag: "SERU 5203",
+    title: "Assessment of Female Genital Schistosomiasis symptoms among adolescent girls in Tana Delta sub-county, Tana River County, Kenya",
+  },
+  {
+    tag: "KNH-UON P130/02/2022",
+    title: "Factors associated with use of long-acting reversible contraceptives among women seeking family planning services at two referral hospitals in Nairobi County, Kenya",
+  },
+];
+
+const COI_STUDIES = [
+  {
+    tag: "SERU 5405",
+    title: "Development of a serological method for spatio-temporal epidemiological monitoring of schistosomiasis transmission: a repeated cross-sectional study in Homa Bay and Kwale counties, Kenya",
+    pi: "PI: Prof. Sammy Njenga & Prof. Shinjiro Hamano",
+  },
+  {
+    tag: "SERU 0000",
+    title: "Field evaluation of a prototype serological rapid diagnostic test for detection of human Schistosoma mansoni infection",
+    pi: "PI: Prof. Sammy Njenga",
+  },
+  {
+    tag: "SERU 5175",
+    title: "Molecular xenomonitoring of lymphatic filariasis in Kenya: a cross-sectional post-treatment surveillance in formerly endemic foci bordering neighbouring countries",
+    pi: "PI: Prof. Sammy Njenga & Wyckliff Omondi",
+  },
+  {
+    tag: "SERU 5153",
+    title: "Development and application of monitoring systems for schistosomiasis infection in intermediate snails, water, and non-human mammals in Homa Bay and Kwale counties",
+    pi: "PI: Prof. Sammy Njenga, Dr. Collins Ouma & Prof. Shinjiro Hamano",
+  },
+  {
+    tag: "SERU 4776",
+    title: "Operational research to guide post-treatment surveillance following administration of triple drug intervention against lymphatic filariasis in Lamu and Jomvu, coastal Kenya",
+    pi: "PI: Prof. Sammy Njenga",
+  },
+];
