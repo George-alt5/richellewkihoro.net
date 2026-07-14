f4e03f88d623f2691eb7530a160d3c4e7739dde0
George Kihoro <georgekihoro6@gmail.com>
2026-07-14 12:22:18 +0300

    Refactor App component to enhance routing structure and improve readability

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -7,3 +7,18 @@ import Speaking from "./pages/Speaking";
 import Contact from "./pages/Contact";
 import "./App.css";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/research" element={<Project />} />
+          <Route path="/speaking" element={<Speaking />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
